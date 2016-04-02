6b9869bc0c5a6cb90a8a63c3f157a541d180f6a7
Dyzr-Daniel <daniel.grossstueck@gmx.de>
2016-04-02 16:30:38 +0200

    Create adblock-detection.js

---
+++ adblock-detection.js
@@ -0,0 +1,6 @@
+//Detects if advert.js is blocked. If script is blocked ->show anti adblock message/image
+if (document.getElementById("tester") == undefined) 
+{ 
+document.write('<a href="http://www.lolchampion.de/adblocker-deaktiveren/">
+<img src="http://www.lolchampion.de/_wordpress_dev716a/wp-content/uploads/2014/12/antiadblock_Amumu.jpg"></a>'); 
+}
